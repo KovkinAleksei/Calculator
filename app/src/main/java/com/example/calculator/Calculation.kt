0a502ed588304dcb59c117911d8ce394eaa52445
Aleksei <aleksey.kovkin@yandex.ru>
2023-02-23 23:30:52 +0700

    Реализованы арифметические операции для небольших чисел, сброс и смена знака.

---
+++ app/src/main/java/com/example/calculator/Calculation.kt
@@ -0,0 +1,135 @@
+package com.example.calculator
+
+class Calculation {
+    private var result = 0.0
+    private var prevNumber = ""
+    private var currentNumber = "0"
+    private var operation = ""
+
+    // Приписывание цифры в конец числа
+    fun addDigit(digit: String) : String {
+        // Удаление незначащих нулей
+        if (currentNumber == "0")
+            currentNumber = ""
+
+        // Ввод числа до девяти знаков
+        if (currentNumber.length < 9)
+            currentNumber += digit
+
+        if (prevNumber == "" || operation == "=")
+            result = currentNumber.toDouble()
+
+        return currentNumber
+    }
+
+    // Перевод Double в String для вывода результата
+    private fun doubleToString(result: Double) : String {
+        // Вывод нуля без минуса
+        if (result.toString() == "-0.0")
+            return "0"
+
+        // Разделение числа на целую и дробную части
+        val strResult = result.toString()
+        var resultParts = strResult.split('.')
+
+        // Ошибка вывода слишком длинного числа
+        if (resultParts[0].length > 9)
+            return "error"
+
+        // Вывод числа без лишней дробной части
+        if (resultParts[1] == "0")
+            return resultParts[0]
+
+        // Вывод числа с дробной частью, укороченной до длины числа в 9 символов
+        if (strResult[8] != ',')
+            return strResult.removeRange(9..strResult.length - 1)
+
+        return strResult.removeRange(8..strResult.length - 1)
+    }
+
+    // Выбор операции
+    fun addOperation(operation: String) : String {
+        // Нахождение результата вычисления
+        getResult(this.operation)
+
+        // Вывод выбранной операции
+        this.operation = operation
+
+        // Продолжение ввода следующего числа
+        if (currentNumber != "") {
+            prevNumber = currentNumber
+            currentNumber = ""
+        }
+
+       return doubleToString(result) + operation
+    }
+
+    // Нахождение результата выражения
+    private fun getResult(operation: String) {
+        if (prevNumber == "") {
+            result = currentNumber.toDouble()
+            return
+        }
+
+        if (currentNumber == "0" && operation == "÷")
+            result = 0.0
+        else if (currentNumber != "" && operation != "=") {
+            result = when (operation) {
+                "+" -> result + currentNumber.toDouble()
+                "-" -> result - currentNumber.toDouble()
+                "×" -> result * currentNumber.toDouble()
+                "÷" -> result / currentNumber.toDouble()
+                else -> 0.0
+            }
+        }
+    }
+
+    // Вывод результата
+    fun showResult() : String {
+        getResult(operation)
+        operation = "="
+
+        currentNumber=doubleToString(result)
+
+        return doubleToString(result)
+    }
+
+    // Отмена действий
+    fun reset() : String {
+        result = 0.0
+        prevNumber = ""
+        currentNumber = "0"
+        operation = ""
+
+        return currentNumber
+    }
+
+    // Смена знака введённого числа или операции
+    fun changeSign() : String {
+        // Смена знака последнего введённого числа
+        if (currentNumber != "") {
+            currentNumber = (currentNumber.toInt() * (-1)).toString()
+
+            if (operation == "=")
+                result *= -1
+
+            return currentNumber
+        }
+
+        // Смена знака последней введённой операции
+        if (operation == "-") {
+            operation = "+"
+
+            return doubleToString(result) + operation
+        }
+
+        if (operation == "+") {
+            operation = "-"
+
+            return doubleToString(result) + operation
+        }
+
+        // Пропуск нажатия кнопки
+        return doubleToString(result) + operation
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,10 +6,112 @@ import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var calculation = Calculation()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Добавление ViewBinding в activity_main
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // Нажатие на кнопку 0
+        binding.button0.setOnClickListener{
+            val digit = binding.button0.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 1
+        binding.button1.setOnClickListener{
+            val digit = binding.button1.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 2
+        binding.button2.setOnClickListener{
+            val digit = binding.button2.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 3
+        binding.button3.setOnClickListener{
+            val digit = binding.button3.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 4
+        binding.button4.setOnClickListener{
+            val digit = binding.button4.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 5
+        binding.button5.setOnClickListener{
+            val digit = binding.button5.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 6
+        binding.button6.setOnClickListener{
+            val digit = binding.button6.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 7
+        binding.button7.setOnClickListener{
+            val digit = binding.button7.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 8
+        binding.button8.setOnClickListener{
+            val digit = binding.button8.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку 9
+        binding.button9.setOnClickListener{
+            val digit = binding.button9.text.toString()
+            binding.result.text = calculation.addDigit(digit)
+        }
+
+        // Нажатие на кнопку сложения
+        binding.plusButton.setOnClickListener {
+            val sign = binding.plusButton.text.toString()
+            binding.result.text = calculation.addOperation(sign)
+        }
+
+        // Нажатие на кнопку вычитания
+        binding.minusButton.setOnClickListener {
+            val sign = binding.minusButton.text.toString()
+            binding.result.text = calculation.addOperation(sign)
+        }
+
+        // Нажатие на кнопку умножения
+        binding.multiplyButton.setOnClickListener {
+            val sign = binding.multiplyButton.text.toString()
+            binding.result.text = calculation.addOperation(sign)
+        }
+
+        // Нажатие на кнопку деления
+        binding.divideButton.setOnClickListener {
+            val sign = binding.divideButton.text.toString()
+            binding.result.text = calculation.addOperation(sign)
+        }
+
+        // Нажатие на кнопку равно
+        binding.equalsButton.setOnClickListener {
+            binding.result.text = calculation.showResult()
+        }
+
+        // Нажатие на кнопку AC
+        binding.resetButton.setOnClickListener {
+            binding.result.text = calculation.reset()
+        }
+
+        // Нажатие на кнопку плюс-минус
+        binding.plusMinusButton.setOnClickListener {
+            binding.result.text = calculation.changeSign()
+        }
     }
 }
